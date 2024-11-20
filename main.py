c068843e4b212b9e61c24b989853e5d97c71e2f2
Wolvinemore <kadinmheacock7@gmail.com>
2024-11-20 17:11:37 -0600

    Overall edits to template

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'sup nerd'
+    @app.route('/hello')
+    def hello():
+        return 'sup nerd'
+    return app
