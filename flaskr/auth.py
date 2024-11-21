46ab5d367753203e028880ec07a423c2738f87ae
Wolvinemore <kadinmheacock7@gmail.com>
2024-11-20 21:41:41 -0600

    Adding logout info

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -66,3 +66,10 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
