032a2dde3cd6593f2cb04fb9d18838b79466deb0
Wolvinemore <kadinmheacock7@gmail.com>
2024-11-23 22:15:30 -0600

    edited gitignore, added to init.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,8 +7,9 @@ def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-
+        # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
+        # store the database in the instance folder
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
@@ -25,10 +26,15 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
+    @app.route('/')
+    def index():
+        return 'Index Page'
 
+    @app.route("/source")
+    def hello():
+        return "It begins!!"
+
+    # register the database commands
     from . import db
 
     db.init_app(app)
@@ -40,6 +46,10 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
 
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
     app.add_url_rule("/", endpoint="index")
 
     return app
