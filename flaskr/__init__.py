7dc5601e1bd9ca9a8c9ca6d81e52a2c3e294860e
Wolvinemore <kadinmheacock7@gmail.com>
2024-12-04 20:36:19 -0600

    added missing imports

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,21 +4,20 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    """Create and configure an instance of the Flask application."""
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY="dev",
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.update(test_config)
+        app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
@@ -26,30 +25,19 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
-    def index():
-        return 'Index Page'
-
-    @app.route("/source")
+    # a simple page that says hello
+    @app.route('/hello')
     def hello():
-        return "It begins!!"
+        return 'Hello, World!'
 
-    # register the database commands
     from . import db
-
     db.init_app(app)
 
-    # apply the blueprints to the app
     from . import auth
-    from . import blog
-
     app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
 
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="index")
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
